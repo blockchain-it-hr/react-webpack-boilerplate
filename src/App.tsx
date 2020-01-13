8bbc164e8c17b1ebb4c9a540e00537d2c392b78a
dark64 <darem966@gmail.com>
2020-01-13 20:49:35 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/app.scss';
+
+const App: React.FC = () => {
+    return (
+        <div id="app">
+            <main role="main">
+                <h1>React Webpack Boilerplate</h1>
+            </main>
+        </div>
+    );
+}
+
+export default App;
